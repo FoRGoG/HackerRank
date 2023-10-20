995e579a233df501f3fe6bd76ff49f8eb547e738
Artyom Filimonov <artyom.filimonov94@gmail.com>
2023-10-20 22:49:31 +0300

    seventh commit

--- Python/04. Sets/10. The Captain's Room.py
+++ Python/04. Sets/10. The Captain's Room.py
@@ -1,8 +1,6 @@
-K = int(input()) # members per group
-unordered_room_numbers = list(map(int, input().split()))
-room_numbers = set(unordered_room_numbers)
-sum1 = sum(unordered_room_numbers)
-sum2 = sum(room_numbers)
-result = sum2*K - sum1
-result1 = result // (K -1)
-print(result1)
+k = int(input())
+line = list(map(int, input().split()))
+setting = set(line)
+temp = sum(setting) * k - sum(line)
+result = temp // (k-1)
+print(result)
